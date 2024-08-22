cbba1ed255f81f86385a2ee17b9691c9d19091f9
Maliheh <mkareshky@yahoo.com>
2024-08-22 18:40:42 +0200

    feat: add chart

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    experimental: {
-      ppr: 'incremental',
-    },
-  }
-  
-  export default nextConfig
+const nextConfig = {};
+
+export default nextConfig;
