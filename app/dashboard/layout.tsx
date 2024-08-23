ae4b5678ef6b792d57e5ab4a089eb3ea9ae7f26e
Maliheh <mkareshky@yahoo.com>
2024-08-23 17:41:51 +0200

    feat: invoces and customers

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,4 +1,4 @@
-import SideNav from "./sidenav";
+import SideNav from '@/components/shared/dashboard/sidenav'
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
